e05ed771815145032df37640a2ebe2a13a525cc2
Ketse <snunleys@gmail.com>
2017-10-22 01:46:26 -0400

    More attributes, color coding sight

--- src/renderer/components/Character/CharacterSheet.vue
+++ src/renderer/components/Character/CharacterSheet.vue
@@ -32,27 +32,74 @@
             <input type="text" class="form-control" id="alignment" v-model="char.info.alignment" placeholder="Alignment">
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="row">
-            <span class="col-sm-2 text-center"><strong><u>Name</u></strong></span>
-            <span class="col-sm-2 text-center"><strong><u>Value</u></strong></span>
-            <span class="col-sm-2 text-center"><strong><u>Check</u></strong></span>
-            <span class="col-sm-2 text-center"><strong><u>Save</u></strong></span>
-            <span class="col-sm-2 text-center"><strong><u>Prof</u></strong></span>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="row">
+              <span class="col-sm-2 text-center"><strong><u>Name</u></strong></span>
+              <span class="col-sm-2 text-center"><strong><u>Value</u></strong></span>
+              <span class="col-sm-2 text-center"><strong><u>Check</u></strong></span>
+              <span class="col-sm-2 text-center"><strong><u>Save</u></strong></span>
+              <span class="col-sm-2 text-center"><strong><u>Prof</u></strong></span>
+            </div>
+            <!-- Stats -->
+            <div class="form-group row" v-for="(value, stat) in char.info.stats" :key="stat">
+              <label for="stat" class="col-sm-2 col-form-label">{{firstLetterCapitalized(stat)}}</label>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" id="stat" placeholder="10" v-model.number="value.val">
+              </div>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" id="" v-bind:class="[value.bcolor ? good : bad]" placeholder="0" v-model.number="value.bonus">
+              </div>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" id="" v-bind:class="[value.scolor ? good : bad]" placeholder="0" v-model.number="value.save">
+              </div>
+              <div class="col-sm-2 text-center">
+                <input class="form-check-input" type="checkbox" v-model.number="value.prof">
+              </div>
+            </div>
+            <hr>
+            <!-- Skills -->
+            <div class="row">
+              <div class="form-group row col-sm-6" v-for="(value, skill) in char.info.skills" :key="skill">
+                <label for="skill" class="col-sm-6 col-form-label">{{firstLetterCapitalized(skill)}}</label>
+                <div class="col-sm-4">
+                  <input type="text" class="form-control" v-bind:class="[value.color ? good : bad]" id="skill" placeholder="10" v-model.number="value.val">
+                </div>
+                <div class="col-sm-2 text-center">
+                  <input class="form-check-input" type="checkbox" v-model.number="value.prof">
+                </div>
+              </div>
+            </div>
+            <!-- -->
           </div>
-          <div class="form-group row" v-for="(value, stat) in char.info.stats" :key="stat">
-            <label for="stat" class="col-sm-2 col-form-label">{{firstLetterCapitalized(stat)}}</label>
-            <div class="col-sm-2">
-              <input type="text" class="form-control" id="stat" placeholder="10" v-model.number="value.val">
+          <div class="col-md-4">
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="armor">Armor Class</label>
+                <input type="text" class="form-control" id="armor" v-model="char.info.armorClass" placeholder="10">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="init">Initiative</label>
+                <input type="text" class="form-control" id="init" v-model="char.info.initiative" placeholder="0">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="speed">Speed</label>
+                <input type="text" class="form-control" id="speed" v-model="char.info.speed" placeholder="30">
+              </div>
             </div>
-            <div class="col-sm-2">
-              <input type="text" class="form-control" id="" placeholder="0" v-model.number="value.bonus">
-            </div>
-            <div class="col-sm-2">
-              <input type="text" class="form-control" id="" placeholder="0" v-model.number="value.save">
-            </div>
-            <div class="col-sm-2 text-center">
-              <input class="form-check-input" type="checkbox" v-model.number="value.prof">
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="mhp">Max Hit Points</label>
+                <input type="text" class="form-control" id="mhp" v-model="char.info.maxHitPoints" placeholder="10">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="hp">Hit Points</label>
+                <input type="text" class="form-control" id="hp" v-model="char.info.currHitPoints" placeholder="0">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="thp">Temp</label>
+                <input type="text" class="form-control" id="thp" v-model="char.info.tempHitPoints" placeholder="0">
+              </div>
             </div>
           </div>
         </div>
@@ -76,6 +123,8 @@ export default {
   },
   data() {
     return {
+      good: "good",
+      bad: "bad",
       char: new Character(),
       store: new Store({
         configName: 'characterSheet',
@@ -104,8 +153,17 @@ export default {
     },
     calculateStats: function() {
       for(let stat in this.char.info.stats) {
-        this.char.info.stats[stat].bonus = (this.char.info.stats[stat].val / 2) - 5;
-        this.char.info.stats[stat].save = this.char.info.stats[stat].prof ? (this.char.info.stats[stat].val / 2) - 3 : (this.char.info.stats[stat].val / 2) - 5;
+        let modifier = this.char.info.stats[stat].val;
+        this.char.info.stats[stat].bonus = (modifier / 2) - 5;
+        this.char.info.stats[stat].save = this.char.info.stats[stat].prof ? (modifier / 2) - 3 : (modifier / 2) - 5;
+        this.char.info.stats[stat].bcolor = this.char.info.stats[stat].bonus >= 0;
+        this.char.info.stats[stat].scolor = this.char.info.stats[stat].save >= 0;
+      }
+      for(let skill in this.char.info.skills) {
+        let lStat = this.char.info.skills[skill].stat;
+        let modifier = this.char.info.stats[lStat].val;
+        this.char.info.skills[skill].val = this.char.info.skills[skill].prof ? (modifier / 2) - 3 : (modifier / 2) - 5;
+        this.char.info.skills[skill].color = this.char.info.skills[skill].val >= 0;
       }
     },
     firstLetterCapitalized: function(str) {
@@ -149,5 +207,13 @@ export default {
   margin-left: 10px;
   margin-right: 10px;
   margin-top: 20px;
+}
+
+.good {
+  border-color: #28a745;
+}
+
+.bad {
+  border-color: #dc3545;
 }
 </style>
--- src/renderer/models/Character.js
+++ src/renderer/models/Character.js
@@ -7,6 +7,12 @@ function Character () {
         school : "",
         background : "",
         alignment : "",
+        armorClass : 10,
+        initiative : 0,
+        speed : 30,
+        maxHitPoints : 10,
+        currHitPoints: 10,
+        tempHitPoints: 0,
         stats : {
             strength : {
                 val : 0,
@@ -36,75 +42,93 @@ function Character () {
         skills : {
             acrobatics : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "dexterity"
             },
             animalHandling : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "wisdom"
             },
             arcana : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "intelligence"
             },
             athletics : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "strength"
             },
             deception : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "charisma"
             },
             history : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "intelligence"
             },
             insight : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "wisdom"
             },
             intimidation : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "charisma"
             },
             investigation : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "intelligence"
             },
             medicine : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "wisdom"
             },
             nature : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "intelligence"
             },
             perception : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "wisdom"
             },
             performance : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "charisma"
             },
             persuasion : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "charisma"
             },
             religion : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "intelligence"
             },
             sleightOfHand : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "dexterity"
             },
             stealth : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "dexterity"
             },
             survival : {
                 val : 0,
-                prof : false
+                prof : false,
+                stat : "wisdom"
             }
         }
     };
