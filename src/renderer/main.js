9d90719deafde96be1c4fc01cd69a0644be50288
Ketse <snunleys@gmail.com>
2017-10-23 21:30:49 -0400

    Temp fix for error in console.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -10,7 +10,22 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+/* Temporary Remove after next 2.5.2 upgrade */
+let originalVueComponent = Vue.component
+Vue.component = function(name, definition) {
+  if (name === 'bFormCheckboxGroup' || name === 'bCheckboxGroup' ||
+      name === 'bCheckGroup' || name === 'bFormRadioGroup') {
+    definition.components = {bFormCheckbox: definition.components[0]}
+  }
+  originalVueComponent.apply(this, [name, definition])
+}
+/* Temporary Remove after next 2.5.2 upgrade */
+
 Vue.use(BoostrapVue);
+
+/* Temporary Remove after next 2.5.2 upgrade */
+Vue.component = originalVueComponent
+/* Temporary Remove after next 2.5.2 upgrade */
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
